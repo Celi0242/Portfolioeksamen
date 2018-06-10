94c676d877ef514df6d3a30b136275c2ed122cd4
Celina Lindqvist <celinalindqvist@Celinas-MacBook-Air.local>
2018-06-10 11:49:38 +0200

    burgermenu

--- js.js
+++ js.js
@@ -1,2 +1,20 @@
 // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 //siden er loadet: sidenVises
+
+$(window).on("load", sidenVises);
+
+
+function sidenVises() {
+    console.log("sidenVises");
+    $(".menubutton").on("click", trykPåMenubutton);
+
+
+}
+
+function trykPåMenubutton() {
+    console.log("trykPåMenubutton");
+    $("nav").toggleClass("hidden");
+    $(".menubutton").toggleClass("kryds");
+
+
+}
